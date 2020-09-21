99c03848da18dc5d936108b09b3e574d2816607a
alazarabraham <amarkos@xpanxion.com>
2020-09-20 23:06:08 -0400

    changed entities

--- src/main/java/com/studentregistration/Program.java
+++ src/main/java/com/studentregistration/Program.java
@@ -10,13 +10,20 @@ import javax.persistence.Id;
 public class Program {
     private Integer programId;
     private String programName;
+    private String instructor;
+    private String semester;
+    private String level;
+
  
     public Program() {
     }
  
-    public Program(Integer programId, String programName) {
+    public Program(Integer programId, String programName,String instructor,String semester,String level) {
         this.programId = programId;
         this.programName = programName;
+        this.instructor = instructor;
+        this.semester = semester;
+        this.level = level;
     }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,6 +41,30 @@ public class Program {
 
 	public void setProgramName(String programName) {
 		this.programName = programName;
+	}
+
+	public String getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getSemester() {
+		return semester;
+	}
+
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
 	}
  
    
--- src/main/java/com/studentregistration/Student.java
+++ src/main/java/com/studentregistration/Student.java
@@ -9,11 +9,12 @@ import javax.persistence.Id;
  
 @Entity
 public class Student {
+	
     private Integer studentId;
     private String firstname;
     private String lastname;
+    private String degreeProgram;
     private String DOB;
-    private Integer programId;
 
 
     
@@ -21,12 +22,13 @@ public class Student {
     public Student() {
     }
  
-    public Student(Integer studentId, String firstname,String lastname,String DOB,Integer programId) {
+    public Student(Integer studentId, String firstname,String lastname,String degreeProgram,String DOB) {
         this.studentId = studentId;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.degreeProgram = degreeProgram;
+
         this.DOB = DOB;
-        this.programId = programId;
 
     }
     @Id
@@ -54,6 +56,16 @@ public class Student {
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+	
+	
+
+	public String getDegreeProgram() {
+		return degreeProgram;
+	}
+
+	public void setDegreeProgram(String degreeProgram) {
+		this.degreeProgram = degreeProgram;
+	}
 
 	public String getDOB() {
 		return DOB;
@@ -63,18 +75,12 @@ public class Student {
 		DOB = dOB;
 	}
 
-	public Integer getProgramId() {
-		return programId;
-	}
-
-	public void setProgramId(Integer programId) {
-		this.programId = programId;
-	}
- 
+	
     
     
 
 	
  
     
+
 }
